9c7d4402e426777401b009eb67dd5f5e124ee751
Joren De Groof <git@jorendegroof.be>
2017-05-03 09:39:48 +0200

    Filters out bots

--- colleagues.rb
+++ colleagues.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   get '/' do
     client = Slack::Web::Client.new
-    @members = client.users_list.members
+    @members = client.users_list.members.select { |m| m.is_bot == false }
     haml :index
   end
 end
